36e2f14cdd4291a6e715ef9bff2a83ac2b920c5c
harshita <harshitakatara34>
2023-04-29 19:45:05 +0530

    d

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const { route } = require("./routes/user.route")
 const { notes } = require("./routes/notes.router")
 const { auth } = require("./Middleware/auth.middleware")
 
+const cors=require("cors")
+
 const app=express()
+app.use(cors())
 app.use(express.json())
 app.use("/",route)
 app.use(auth)
